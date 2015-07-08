c3b0974df744dad15c458a006a151b2a3528027a
malnick <malnick@gmail..com>
2015-07-08 13:15:59 -0700

    no init

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,17 +20,20 @@ class marathon_template (
     content   => template($template_location),
     mode      => '0644',
   }
-  ->
-  file { '/etc/init.d/marathon-template-init':
-    ensure    => file,
-    source    => 'puppet:///modules/marathon_template/init.sh',
-    mode      => '0755',
-  }
+#  ->
+#  file { '/etc/init.d/marathon-template-init':
+#    ensure    => file,
+#    source    => 'puppet:///modules/marathon_template/init.sh',
+#    mode      => '0755',
+#  }
+#  -> 
+#  service { 'marathon-template-init':
+#    ensure      => running,
+#    hasrestart  => true,
+#    enable      => true,
+#  }
   -> 
-  service { 'marathon-template-init':
-    ensure      => running,
-    hasrestart  => true,
-    enable      => true,
+  exec {'marathon-template':
+    command => '/usr/local/bin/marathon-template'
   }
-
 }
